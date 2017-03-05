7451b5d68a36322e9a00a48f764fef17984ba509
zabulskyy <v.zabulskyy@gmail.com>
2017-03-05 21:34:34 +0200

    Update test_zip_process.py

--- lab_5_aud/zip/test_zip_process.py
+++ lab_5_aud/zip/test_zip_process.py
@@ -1,6 +1,4 @@
 from scale_zip import *
 
 if __name__ == "__main__":
-	ScaleZip(input("please, type zip-file name: ").replace('.zip','') + '.zip').process_zip()
-	
-	
+    ScaleZip(input("please, type zip-file name: ").replace('.zip','') + '.zip').process_zip()	
