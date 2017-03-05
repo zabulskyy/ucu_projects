69f9ccdfad8595de0a7114ce53634bba21be0478
zabulskyy <v.zabulskyy@gmail.com>
2017-03-05 21:33:34 +0200

    Update scale_zip.py

--- lab_5_aud/zip/scale_zip.py
+++ lab_5_aud/zip/scale_zip.py
@@ -6,10 +6,10 @@ from pygame.transform import scale
 from PIL import Image
 
 class ScaleZip(ZipProcessor):
-	def process_files(self):
-		'''Scale each image in the directory to 640x480'''
-		for filename in os.listdir(self.temp_directory):
-			im = image.load(self._full_filename(filename))
-			scaled = scale(im, eval(input('please set size (width, heigt) as in example: (1024,720): ').replace(' ',',')))
-			image.save(scaled, self._full_filename(filename))
+    def process_files(self):
+        '''Scale each image in the directory to 640x480'''
+        for filename in os.listdir(self.temp_directory):
+            im = image.load(self._full_filename(filename))
+            scaled = scale(im, eval(input('please set size (width, heigt) as in example: (1024,720): ').replace(' ',',')))
+            image.save(scaled, self._full_filename(filename))
 
